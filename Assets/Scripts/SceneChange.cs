7770851f4b6c705ab725cf6fb13f378e29b88a90
TheBlaze <santi.volcanos@gmail.com>
2023-07-18 02:01:35 -0600

    60%

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    private Collider2D col;
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        col = GetComponent<Collider2D>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Slime.cs
+++ Assets/Scripts/Slime.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.UIElements;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Slime : Enemy
@@ -10,12 +10,14 @@ public class Slime : Enemy
     public float chaseRadius;
     public float attackRadius;
     public Transform homePosition;
+    public Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         target = GameObject.FindWithTag("Player").transform;
         currentstate = EnemyState.idle;
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -29,12 +31,47 @@ public class Slime : Enemy
         {
             if (currentstate==EnemyState.idle ||currentstate==EnemyState.walk && currentstate!=EnemyState.stagger)
             {
-
+                Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+                changeAnim(temp-transform.position);
+                myRigidbody.MovePosition(temp);
+                ChangeState(EnemyState.walk);
+                anim.SetBool("moving", true);
             }
-            Vector3 temp = Vector3.MoveTowards(transform.position, target.position,moveSpeed*Time.deltaTime);
-            myRigidbody.MovePosition(temp);
-            ChangeState(EnemyState.walk);
+            else
+            {
+                anim.SetBool("moving", false);
+            }
         }
+    }
+    private void SetAnimFloat(Vector2 setVector) 
+    {
+        anim.SetFloat("movex",setVector.x);
+        anim.SetFloat("movey",setVector.y);
+    }
+    private void changeAnim(Vector2 direction) 
+    {
+        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+        {
+            if (direction.x>0)
+            {
+                SetAnimFloat(Vector2.right);
+            }
+            else if (direction.x<0)
+            {
+                SetAnimFloat(Vector2.left);
+            }
+        }
+        else if (Mathf.Abs(direction.x)<Mathf.Abs(direction.y))
+        {
+            if (direction.y > 0)
+            {
+                SetAnimFloat(Vector2.up);
+            }
+            else if (direction.y < 0)
+            {
+                SetAnimFloat(Vector2.down);
+            }
+        } 
     }
     private void ChangeState(EnemyState newState)
     {
